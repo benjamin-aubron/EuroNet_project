b363aeb330f011f1bd5d87673d57b51104060894
Benjamin AUBRON <benjamin.aubron@outlook.com>
2023-06-27 15:31:20 +0200

    sankey diagram

--- public_institutions_dashboard/dashboard.js
+++ public_institutions_dashboard/dashboard.js
@@ -1,36 +1,65 @@
-
-
-
-
 // Sankey Diagram
 var data = {
   type: "sankey",
   orientation: "h",
   node: {
-     pad: 15,
-     thickness: 30,
-     line: {
-        color: "black",
-        width: 0.5
-     },
-     label: ["Institutions publiques", "Entreprises", "Citoyens", "Organismes à but non lucratif", "Entité", "Institutions publiques", "Entreprises", "Citoyens", "Organismes à but non lucratif"],
-     color: ["blue", "red", "green", "yellow", "black", "blue", "red", "green", "yellow"]
-        },
+    pad: 15,
+    thickness: 100,
+    line: {
+      color: "black",
+      width: 0.5,
+    },
+    label: [
+      "Institutions publiques",
+      "Entreprises",
+      "Citoyens",
+      "Organismes à but non lucratif",
+      "Entité",
+      "Institutions publiques",
+      "Entreprises",
+      "Citoyens",
+      "Organismes à but non lucratif",
+    ],
+    x: [0, 0, 0, 0, 0.5, 1, 1, 1, 1],
+    y: [0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4],
+    color: [
+      "blue",
+      "red",
+      "green",
+      "yellow",
+      "black",
+      "blue",
+      "red",
+      "green",
+      "yellow",
+    ],
+  },
 
   link: {
-     source: [0,1,2,3,4,4,4,4],
-     target: [4,4,4,4,5,6,7,8],
-     value:  [8,4,2,8,8,12,2,1]
-  }
-  }
+    source: [0, 1, 2, 3, 4, 4, 4, 4],
+    target: [4, 4, 4, 4, 5, 6, 7, 8],
+    value: [8, 4, 2, 8, 8, 12, 2, 1],
+    color: [
+      "blue",
+      "red",
+      "green",
+      "yellow",
+      "blue",
+      "red",
+      "green",
+      "yellow",
+    ],
+  },
+};
 
-  var data = [data]
+var data = [data];
 
-  var layout = {
+var layout = {
   title: "",
   font: {
-     size: 20
-  }
-  }
+    size: 10,
+  },
+  plot_bgcolor: "red",
+};
 
-  Plotly.react('sankey_diagram', data, layout)
+Plotly.react("sankey_diagram", data, layout, {displayModeBar: false}, {responsive: true});
